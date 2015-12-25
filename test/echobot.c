d77725df04e8fa16fba2abcc36670f8fd3020da2
Elmurod Talipov <elmurod.talipov@gmail.com>
2015-12-25 18:53:26 +0900

    add sending receiving and sending messages

--- test/echobot.c
+++ test/echobot.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <telebot-common.h>
 #include <telebot-api.h>
+
+static void update_cb(const telebot_message_t *message)
+{
+    telebot_error_e ret;
+    char str[TELEBOT_MESSAGE_TEXT_SIZE];
+    char chat_id[32];
+    if (strstr(message->text, "/start")) {
+        snprintf(str, TELEBOT_MESSAGE_TEXT_SIZE, "Hello %s",
+                message->from.first_name);
+    }
+    else {
+        snprintf(str, TELEBOT_MESSAGE_TEXT_SIZE, "RE:%s", message->text);
+    }
+    snprintf(chat_id, 32, "%d", message->chat.id);
+
+    ret = telebot_send_message(chat_id, str, "", false, 0, "");
+    if (ret != TELEBOT_ERROR_NONE) {
+        printf("Failed to send message: %d \n", ret);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -39,8 +60,14 @@ int main(int argc, char *argv[])
     printf("First Name: %s\n", me->first_name);
     printf("Last Name: %s\n", me->last_name);
     printf("User Name: %s\n", me->username);
-
     free(me);
+
+    telebot_start(update_cb);
+
+    // wait for callbacks;
+    while (1);
+
+    telebot_stop();
     telebot_destroy();
 
     return 0;
